c73e08ff7c17e8546e3b668a9f3f5444ddad041d
Aapo Talvensaari <aapo.talvensaari@gmail.com>
2015-09-16 20:59:37 +0300

    Combined group validator to validation.lua.

--- lib/resty/validation.lua
+++ lib/resty/validation.lua
@@ -243,8 +243,54 @@ local validators = setmetatable({
     reverse      = factory.reverse()
 }, factory)
 
+local rmt = {
+    __tostring = function(self)
+        return self.value
+    end
+}
+local mt = {}
+
+function mt:__call(t)
+    local errors  = {}
+    local results = {}
+    for index, value in pairs(t) do
+        local ok, err = true, value
+        if self[index] then
+            ok, err = self[index](value)
+        end
+        if ok then
+            results[index] = setmetatable({
+                name = name,
+                input = value,
+                value = err,
+                valid = true,
+                invalid = false,
+                error = nil
+            }, rmt)
+        else
+            errors[#errors + 1] = index
+            errors[index] = err
+            results[index] = setmetatable({
+                name = name,
+                input = value,
+                value = value,
+                valid = false,
+                invalid = true,
+                error = err
+            }, rmt)
+        end
+    end
+    return #errors == 0, results, errors
+end
+
+local group = {}
+
+function group.new()
+    return setmetatable({}, mt)
+end
+
 local function validation(func, parent_f, parent, method)
-    return setmetatable({ validators = validators }, {
+    return setmetatable({ new = group.new, validators = validators }, {
         __index = function(self, index)
             return validation(function(...)
                 local valid, value = func(...)
